11aae9dfe024b423740b2987f6d45868ec61bb11
navid015 <navid_pme@hotmail.com>
2024-04-29 18:31:45 -0500

    1st edit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import MountainPng from "../../assets/moon-surface-hd.png";
+
+const Hero = () => {
+  return (
+    <div className=" bg-black/20 h-full">
+      <div className="h-full flex justify-center items-center p-4">
+        <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="text-white space-y-4 lg:pr-36">
+            <h1 data-aos="fade-up" className="text-5xl font-bold">
+              LET'S EXPLORE THE UNIVERSE
+            </h1>
+            <p data-aos="fade-up" data-aos-delay="300">
+            The universe is all of space and time and their contents. It comprises all of existence, any fundamental interaction, physical process and physical constant, and therefore all forms of energy and matter, and the structures they form, from sub-atomic particles to entire galaxies. 
+            </p>
+            <button
+              data-aos="fade-up"
+              data-aos-delay="500"
+              className="bg-blue-400 text-white hover:bg-blue-500 px-4 py-1 rounded-md duration-200"
+            >
+              LEARN MORE
+            </button>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <img
+        src={MountainPng}
+        alt=""
+        className="absolute right-0 bottom-0 w-full brightness-50 z-10"
+      />
+
+      <div className="absolute bottom-0 z-30 right-0 w-full bg-gradient-to-b from-transparent from-10% to-primary to-90% h-[20px] sm:h-[50px] md:[60px]"></div>
+    </div>
+  );
+};
+
+export default Hero;
