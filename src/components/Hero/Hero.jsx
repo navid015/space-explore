1596d7bc0a2f333e3f505a5c86b232ed96f29adf
navid015 <navid_pme@hotmail.com>
2024-04-30 00:25:46 -0500

    commit after chnaging hero comp h1 and text color

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,16 +7,16 @@ const Hero = () => {
       <div className="h-full flex justify-center items-center p-4">
         <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4">
           <div className="text-white space-y-4 lg:pr-36">
-            <h1 data-aos="fade-up" className="text-5xl font-bold">
+            <h1 data-aos="fade-up" className="text-5xl font-bold text-orange-600">
               LET'S EXPLORE THE UNIVERSE
             </h1>
-            <p data-aos="fade-down" data-aos-delay="300">
+            <p className="text-orange-400" data-aos="fade-down" data-aos-delay="300">
               The universe is all of space and time and their contents. It comprises all of existence, any fundamental interaction, physical process and physical constant, and therefore all forms of energy and matter, and the structures they form, from sub-atomic particles to entire galaxies.
             </p>
             <button
               data-aos="fade-up"
               data-aos-delay="400"
-              className="bg-blue-400 text-white hover:bg-blue-700 px-6 py-2 rounded-md cursor-pointer transition-colors duration-300"
+              className="bg-orange-200 text-black font-semibold hover:bg-blue-700 px-6 py-2 rounded-md cursor-pointer transition-colors duration-300"
             >
               LEARN MORE
             </button>
